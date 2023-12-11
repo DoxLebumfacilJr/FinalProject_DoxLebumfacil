97576d068192c75d335af4f153db8dbcdfed2c65
MrDTheGreat <darylekazuto@gmail.com>
2023-12-11 22:44:40 +0800

    Sample Comit

---
+++ screens/Welcome.js
@@ -0,0 +1,154 @@
+import { View, Text, Pressable, Image, ImageBackground } from 'react-native'
+import React from 'react'
+import { LinearGradient } from "expo-linear-gradient";
+import COLORS from '../constants/colors';
+import Button from '../components/Button';
+
+const Welcome = ({ navigation }) => {
+
+    return (
+        <LinearGradient
+            style={{
+                flex: 1,
+            }}
+            colors={[COLORS.secondary, COLORS.primary]}
+        >
+            <View style={{ flex: 1 }}>
+                <View>
+                
+                    <Image
+                        source={require("../assets/Shadow.png")}
+                        style={{
+                            height: 100,
+                            width: 100,
+                            borderRadius: 20,
+                            position: "absolute",
+                            top: 10,
+                            transform: [
+                                { translateX: 20 },
+                                { translateY: 50 },
+                                { rotate: "-15deg" }
+                            ]
+                        }}
+                    />
+
+                    <Image
+                        source={require("../assets/ShadowFolder.png")}
+                        style={{
+                            height: 100,
+                            width: 100,
+                            borderRadius: 20,
+                            position: "absolute",
+                            top: -30,
+                            left: 100,
+                            transform: [
+                                { translateX: 50 },
+                                { translateY: 50 },
+                                { rotate: "-5deg" }
+                            ]
+                        }}
+                    />
+
+                    <Image
+                        source={require("../assets/Overlord.png")}
+                        style={{
+                            width: 100,
+                            height: 100,
+                            borderRadius: 20,
+                            position: "absolute",
+                            top: 130,
+                            left: -50,
+                            transform: [
+                                { translateX: 50 },
+                                { translateY: 50 },
+                                { rotate: "15deg" }
+                            ]
+                        }}
+                    />
+
+                    <Image
+                        source={require("../assets/CuteMikasa.png")}
+                        style={{
+                            height: 235,
+                            width: 200,
+                            borderRadius: 20,
+                            position: "absolute",
+                            top: 110,
+                            left: 100,
+                            transform: [
+                                { translateX: 50 },
+                                { translateY: 50 },
+                                { rotate: "-15deg" }
+                            ]
+                        }}
+                    />
+                </View>
+
+                {/* content  */}
+
+                <View style={{
+                    paddingHorizontal: 22,
+                    position: "absolute",
+                    top: 400,
+                    width: "100%"
+                }}>
+                    <Text style={{
+                        fontSize: 36,
+                        fontWeight: 800,
+                        color: COLORS.white
+                    }}>Welcome To Paradox</Text>
+                    <Text style={{
+                        fontSize: 20,
+                        fontWeight: 100,
+                        color: COLORS.white
+                    }}>So What Are You Waiting For Let's Get Started</Text>
+
+                    <View style={{ marginVertical: 22 }}>
+                        <Text style={{
+                            fontSize: 16,
+                            color: COLORS.white,
+                            marginVertical: 4
+                        }}>Connect With Us So You Can See More</Text>
+                        <Text style={{
+                            fontSize: 16,
+                            color: COLORS.white,
+                        }}>Anime, Manga And Manhuas Etc. So Enjoy Join Now!!🤣🤣🤣</Text>
+                    </View>
+
+                    <Button
+                        title="Join Now"
+                        onPress={() => navigation.navigate("Signup")}
+                        style={{
+                            marginTop: 22,
+                            width: "100%"
+                        }}
+                    />
+
+                    <View style={{
+                        flexDirection: "row",
+                        marginTop: 12,
+                        justifyContent: "center"
+                    }}>
+                        <Text style={{
+                            fontSize: 16,
+                            color: COLORS.white
+                        }}>Already have an account ?</Text>
+                        <Pressable
+                            onPress={() => navigation.navigate("Login")}
+                        >
+                            <Text style={{
+                                fontSize: 16,
+                                color: COLORS.white,
+                                fontWeight: "bold",
+                                marginLeft: 4
+                            }}>Login</Text>
+                        </Pressable>
+
+                    </View>
+                </View>
+            </View>
+        </LinearGradient>
+    )
+}
+
+export default Welcome
